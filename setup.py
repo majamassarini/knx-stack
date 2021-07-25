0635321ee65ae73a40e830c5096174ae6a6dfa35
majamassarini <maja.massarini@gmail.com>
2021-07-26 00:18:38 +0200

    Improved coverage

--- setup.py
+++ setup.py
@@ -2,12 +2,21 @@ from setuptools import setup, find_packages
 
 setup(name="knx-stack",
       version="0.9",
-      description="Parse and send knx messages",
-      url="https://bitbucket.org/culsucar/knxstack",
-      long_description="",
+      description="A python3 KNX stack for USB HID and KNXnet IP",
+      url="https://github.com/majamassarini/knx-stack",
+      long_description="A python3 KNX stack for USB HID and KNXnet IP, "
+                       "which can be used both in a synchronous and asynchronous client.",
       author="Maja Massarini",
       author_email="maja.massarini@gmail.com",
-      license="All rights reserved",
+      license="MIT",
+      classifiers=[
+            "Development Status :: 3 - Alpha",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: POSIX :: Linux",
+            "Programming Language :: Python :: 3.8",
+            "Topic :: Communications",
+            "Intended Audience :: Developers",
+      ],
       packages=find_packages(exclude=[]),
       include_package_data=True,
       )
